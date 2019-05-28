ae4cd3cc9890cc5051764735f75b2b164f9afef5
Douglas Azevedo <douglaaa.s@hotmail.com>
2019-05-28 19:53:05 -0300

    'nivelamento'

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -7,14 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  public vetor = [];
+  public bolha = { i: 0, t: 0, c: 0 };
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
   }
 
+  // var vetorQuebrado = vetor.trim().split(',',';',' ');
 
-  ordenarVetor(){
-    
+
+  ordenarVetor(vetor){
+    this.bolha.i = vetor.length -1;
+    let tamanho = vetor.length-1;
+    let tamanhoB = vetor.length-1;
+    let temp1 = 0;
+    let temp2 = 0;
+
+    for(let i = 0;i < tamanho;i++){
+      for(let x = 0;x < tamanhoB;x++){
+          if(vetor[x] > vetor[x+1]){
+            temp1 = vetor[x];
+            temp2 = vetor[x+1];
+            vetor[x] = temp2;
+            vetor[x+1] = temp1;
+            this.bolha.c++;
+            this.bolha.t++;
+          }else{
+            this.bolha.c++;
+          } 
+      }
+      tamanhoB--;
+      console.table(vetor);
+    }
   }
 
   ngOnInit() {
