6713e672e54733b5c135a4d2c6bf7c5847744bd9
Douglas Azevedo <douglaaa.s@hotmail.com>
2019-05-29 21:21:41 -0300

    'design'

--- src/app/list/ListPage.ts
+++ src/app/list/ListPage.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -14,18 +15,11 @@ export class ListPage implements OnInit {
   public bolha = { i: 0, t: 0, c: 0 };
   public items: Array<{ title: string; note: string; icon: string }> = [];
 
-  constructor(public alert: AlertController) {
+  constructor(public alert: AlertController,public toastSucess: ToastController) {
   }
-  // public alerta = this.alert.create({
-  //   header: 'Alert',
-  //   subHeader: 'Subtitle',
-  //   message: 'This is an alert message.',
-  //   buttons: ['Cancel', 'Open Modal', 'Delete']
-  // });
-
    async verificaEntrada(vetor) {
     const alerta = await this.alert.create({
-      header: 'ERRO PNC',
+      header: 'Atenção',
       message: 'Só números são permitidos!',
       buttons: ['Ok']
     });
@@ -34,17 +28,20 @@ export class ListPage implements OnInit {
       alerta.present();
       return
     }
-
   }
-
+  async toast(){
+    const toast = await this.toastSucess.create({
+      message: 'Ação concluída!',
+      duration: 2000
+    });
+    toast.present();
+  } 
   parsePaNumero(vetor) {
-
     for(let i = 0; i < vetor.length; i++) {
       vetor[i] = parseInt(vetor[i]);
     }
     return vetor;
   }
-
 
   ordenarVetor(vetor) {
     let vetorQuebrado = vetor.trim().split((/[,;\s]+/));
@@ -70,13 +67,10 @@ export class ListPage implements OnInit {
         }
       }
       tamanhoB--;
-      console.table(vetorQuebrado);
     }
   }
 
-  ngOnInit() {
-    console.log('Oninit');
-  }
+  ngOnInit() {}
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
